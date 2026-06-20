a4f5ccf7b8143791560c65728d659c43df34b464
RahaBandeh <rahabande29@gmail.com>
2026-06-20 10:20:35 +0330

    change

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(){
         System.out.println("Raha Bandeh is god object!");
-        System.out.println(20);
+        System.out.println(21);
     }
 }
